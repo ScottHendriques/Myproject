95d4545e20cd7c541f0f352be1c59ee8a5f0aebb
ScottHendriques <162937244+ScottHendriques@users.noreply.github.com>
2025-03-05 13:00:07 +0530

    StationCapabilities v2

--- frontend/src/pages/Stationcapabilities.jsx
+++ frontend/src/pages/Stationcapabilities.jsx
@@ -18,18 +18,135 @@ const data = {
             title: "Area Manager Cargo",
             phone: "02 - 5112108",
             email: "hbahaj@etihad.ae",
+            address: "Etihad Airways Headquarters Khalifa City A",
+            website: "www.etihadcargo.com",
+            hours: "Open 08:00 AM - 05:00 PM",
           },
           {
             name: "Rizza Jebulan",
             title: "Key Account Officer",
             phone: "+971 -02-5116823",
             email: "rjebulan@etihad.ae",
+            address: "Etihad Airways Headquarters Khalifa City A",
+            website: "www.etihadcargo.com",
+            hours: "Open 08:00 AM - 05:00 PM",
           },
           {
             name: "Chris Eddam Curado Po",
             title: "VIP Desk",
             phone: "02-5511183",
             email: "CEPo@etihad.ae",
+            address: "Etihad Airways Headquarters Khalifa City A",
+            website: "www.etihadcargo.com",
+            hours: "Open 08:00 AM - 05:00 PM",
+          },
+        ],
+      },
+      "Dubai": {
+        agent: "Dnata",
+        location: "Airport Road, Dubai",
+        phone: "+971 4 2111111",
+        email: "dnatacargodxb@dnata.com",
+        website: "www.dnata.com/en/global-network/united-arab-emirates",
+        hours: "Open 12:00 AM - 11:59 PM",
+        contacts: [
+          {
+            name: "Rajiv Kumar / Samina Khan",
+            title: "Sales Managers",
+            phone: "+971 02 5990099, +971 4 4072273",
+            email: "DXBcargosales@etihad.ae",
+            address: "Mazaya Centre Sheikh Zayed road.",
+            website: "www.etihadcargo.com",
+            hours: "Open 09:00 AM - 05:00 PM",
+          },
+        ],
+      },
+      "Sharjah": {
+        agent: "Cargo Operations Manager",
+        location: "Sharjah",
+        phone: "+9715141173, +971501166258",
+        email: "cgopsmgr@sharjahaviation.com",
+        website: "www.sharjahop/en/global-network/united-arab-emirates",
+        hours: "Open 12:00 AM - 11:59 PM",
+        contacts: [
+          {
+            name: "Jamshad Gull (export) / Tariq Al Mahri (import)",
+            title: "Cargo Representatives",
+            phone: "+971 02 5990099, +971 4 4072273",
+            email: "DXBcargosales@etihad.ae, TAlmahri@etihad.ae",
+            address: "Mazaya Centre Sheikh Zayed road.",
+            website: "www.etihadcargo.com",
+            hours: "Open 09:00 AM - 05:00 PM",
+          },
+        ],
+      },
+    },
+  },
+  India: {
+    states: {
+      "Mumbai": {
+        agent: "Mumbai International Airport Limited",
+        location: "Air Cargo Complex , Sahar , Andheri (East) , Mumbai 400 099",
+        phone: "+91 7506025997, +91 9167213667",
+        email: "Export.EYmial@gvk.com, import.eymial@gvk.com",
+        website: "www.mialiteworld.gvk.com",
+        hours: "Open 12:00 AM - 11:59 PM",
+        contacts: [
+          {
+            name: "AKSHAY BANGAR",
+            title: "Account Manager (Etihad Cargo)",
+            phone: "9821351265",
+            email: "abangar@etihad.ae",
+            address: "Unit No 902, 9th Floor,Antariksh Thakur House, Makwana Road, Marol, Andheri (East), Mumbai 400059, India",
+            website: "www.etihadcargo.com",
+            hours: "Open 09:30 AM - 06:30 PM",
+          },
+          {
+            name: "ASIM SAYED",
+            title: "Country Manager",
+            phone: "9867380384",
+            email: "asayed@etihad.ae",
+            address: "Unit No 902, 9th Floor,Antariksh Thakur House, Makwana Road, Marol, Andheri (East), Mumbai 400059, India",
+            website: "www.etihadcargo.com",
+            hours: "Open 09:00 AM - 05:30 PM",
+          },
+        ],
+      },
+      "Bengaluru": {
+        agent: "WFS (Bengaluru) Private Limited",
+        location: "Administration Block, Kempegowda International Airport, Devanahalli, Bengaluru",
+        phone: "+91 9538100095",
+        email: "Dmimport.blr@wfs.aero",
+        website: "https://www.wfs.aero/locations/bangalore-india/",
+        hours: "Open 12:00 AM - 11:59 PM",
+        contacts: [
+          {
+            name: "Uday Kumar",
+            title: "Sales Manager – Bengaluru",
+            phone: "+91 80 2768 8393",
+            email: "Uputtaswamygowda@etihad.ae",
+            address: "#202, 2nd Floor, WFS (Bangalore) Pvt Ltd, Kempegowda International Airport, Bangalore-560300",
+            website: "www.etihadcargo.com",
+            hours: "Open 09:00 AM - 05:30 PM",
+          },
+        ],
+      },
+      "Thiruvananthapuram": {
+        agent: "AIR INDIA SATS AIRPORT SERVICE PVT LTD",
+        location: "1st floor Panachamoottil Square, Airport Rd, Vallakadavu, Thiruvananthapuram",
+        phone: "91 471 2461900, 91 471 2505161",
+        email: "eytrv@avscargo.com, Cargooa.Trv@aisats.in, Cargosvcs.trv@aisats.in",
+        website: "www.aisats.in",
+        hours: "Open 09:00 AM - 06:00 PM",
+        contacts: [
+          {
+            name: "Asim Rizvi Sayed",
+            title: "Cargo Country Manager India",
+            phone: "+919 86 7380384",
+            email: "asayed@etihad.ae",
+            address: "Etihad Cargo Unit no 902, 9th Floor, Antariksh Bldg, Makwana Rd, Marol, Andheri East, Mumbai- 400059",
+            website: "www.etihadcargo.com",
+            hours: "Open 09:00 AM - 05:30 PM",
           },
         ],
       },
@@ -46,7 +163,7 @@ const StationCapabilities = () => {
   const stationInfo = data[country].states[state];
 
   return (
-    <div className="flex flex-col items-center p-10 min-h-screen transition-colors bg-base-100 text-base-content w-full">
+    <div className="flex flex-col items-center mt-10 p-10 min-h-screen transition-colors bg-base-100 text-base-content w-full">
       <h2 className="text-3xl font-light">Global</h2>
       <h1 className="text-5xl italic font-semibold">station capabilities</h1>
       <h3 className="text-xl font-bold mt-6">Find a cargo office</h3>
